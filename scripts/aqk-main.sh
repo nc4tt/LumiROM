f4997d7a3c9631c7875665aeb0cd6fe8a1b9806c
Nguyen Cao An Tuong <anntuongg@gmail.com>
2026-04-03 22:56:00 +0700

    re-new link

--- scripts/aqk-main.sh
+++ scripts/aqk-main.sh
@@ -57,7 +57,7 @@ DOWNLOAD_FIRMWARE() {
     echo "Downloading ROM images for $STOCK_DEVICE"
 
     if [[ "$STOCK_DEVICE" == "SM-A325F" || "$STOCK_DEVICE" == "SM-A325M" || "$STOCK_DEVICE" == "SM-M325F" ]]; then
-        gdown 1-mi2v5TV5qmNRb-eXMNalKELmTvL6eyC -O "${DOWN_DIR}/SM-A346E_OneUi85_firmware.zip"
+        gdown 1j_iVNugaOKvUP-HZek_9b81uFJjFQ0D0 -O "${DOWN_DIR}/SM-A346E_OneUi85_firmware.zip"
     elif [[ "$STOCK_DEVICE" == "SM-A225F" || "$STOCK_DEVICE" == "SM-A225M" || "$STOCK_DEVICE" == "SM-E225F" || "$STOCK_DEVICE" == "SM-M225F" || "$STOCK_DEVICE" == "SM-A226B" ]]; then
         CURRENT_LINK=$(printf "%s" "$DOWNLOAD_LINK" | sha256sum | cut -d ' ' -f 1)
 
